5c3020904b092ace00986db64715deae50e589c2
peng-yiwen <76857549+peng-yiwen@users.noreply.github.com>
2025-06-17 17:38:31 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 printf '==========Running==========\n'
 python main.py --kb1 data/D_W_15K_V1/dbp_15K_V1.ttl --kb2 data/D_W_15K_V1/wd_15K_V1.ttl --save_file dw-v1.ttl --alpha 3.0 --init 0.7 
-python main.py --kb1 data/D_W_15K_V1/dbp_15K_V2.ttl --kb2 data/D_W_15K_V2/wd_15K_V2.ttl --save_file dw-v2.ttl --alpha 3.0 --init 0.7
+python main.py --kb1 data/D_W_15K_V2/dbp_15K_V2.ttl --kb2 data/D_W_15K_V2/wd_15K_V2.ttl --save_file dw-v2.ttl --alpha 3.0 --init 0.7
 python main.py --kb1 data/DBP15k/fr_en/dbp-fr.ttl --kb2 data/DBP15k/fr_en/dbp-en.ttl --save_file dbp-fr-en.ttl --alpha 3.0 --init 0.7
 python main.py --kb1 data/DBP15k/zh_en/dbp-zh.ttl --kb2 data/DBP15k/zh_en/dbp-en.ttl --save_file dbp-zh-en.ttl --alpha 3.0 --init 0.7
 python main.py --kb1 data/DBP15k/ja_en/dbp-ja.ttl --kb2 data/DBP15k/ja_en/dbp-en.ttl --save_file dbp-ja-en.ttl --alpha 3.0 --init 0.7
